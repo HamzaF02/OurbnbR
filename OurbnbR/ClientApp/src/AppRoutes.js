b8ef7e2b7a2da69dfa58ddc5eaca567722a895d8
ylli <s364574@oslomet.no>
2023-11-07 16:29:36 +0100

    rental

--- OurbnbR/ClientApp/src/AppRoutes.js
+++ OurbnbR/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { Rental } from "./components/Rental";
 
 const AppRoutes = [
   {
@@ -14,6 +15,9 @@ const AppRoutes = [
   {
     path: '/fetch-data',
     element: <FetchData />
+  }, {
+    path: '/rental',
+    element: <Rental />
   }
 ];
 
---
+++ OurbnbR/ClientApp/src/components/Rental.js
@@ -0,0 +1,39 @@
+﻿import React, { Component } from 'react';
+import "./rental.css";
+
+
+export class Rental extends Component {
+    static displayName = Rental.name;
+
+    render() {
+        return (
+
+          <>
+
+                <h1>List of Rentals</h1>
+
+                <a className="btn btn-light text-dark" asp-action="Table">Table View</a>
+
+
+                    <div className="row col  row-cols-xl-3 row-cols-lg-3  row-cols-md-2  row-cols-sm-1 ">
+                       
+                       
+                          
+
+                       
+                        <a asp-controller="Rental" asp-action="Create" className="link-dark">
+
+                            <div className="d-flex new justify-content-between mt-1" >
+                                <div className="col addcard" >
+                                    <h1 className="add">+</h1>
+                                    <p className="add">Post new Rental!</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+    
+                </>
+       );
+    }
+}
+
