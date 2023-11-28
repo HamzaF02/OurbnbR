b4a86e55986f7f8af623066bc0d3a061f5f4f045
Hamza Farah <89578136+HamzaF02@users.noreply.github.com>
2023-11-28 16:53:30 +0100

    boom

--- OurbnbR/ClientApp/src/components/rental/Details.js
+++ OurbnbR/ClientApp/src/components/rental/Details.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
-<<<<<<< HEAD
-import "./rental.css";
-=======
 import { parseDateTime, parseNumber, parsePrice } from '../../formating';
 
->>>>>>> damn
+import "./rental.css";
 
 export function Details() {
     const [rental,setRental] = useState();
